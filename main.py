a61493f814b26b7e36dc41342a392ad3edfbdfde
Jimishi <>
2021-04-07 00:18:46 +0000

    Uses pysndfx

--- main.py
+++ main.py
@@ -1,18 +1,46 @@
 import discord
+import os
+import youtube_dl
+#from pydub import AudioSegment
+from discord.ext import commands
+#from audio import speed_change
+from pysndfx import AudioEffectsChain
 
-client = discord.Client()
+bot = commands.Bot(command_prefix = '!yee ')
 
-@client.event
+@bot.command(name = 'url')
+async def yt(ctx, url):
+  song_there = os.path.isfile('./song.mp3')
+  try:
+    if song_there:
+      os.remove('./song.mp3')
+  except PermissionError:
+    await ctx.send("dude im going to cuhm on u")
+  ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl': './song.mp3',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+  with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])
+  fx = (
+    AudioEffectsChain()
+    .reverb()
+    .speed(0.77)
+  )
+  fx('./song.mp3', './song.mp3')
+  await ctx.send(file=discord.File('./song.mp3'))
+
+@bot.command(name = 'ping')
+async def ping(ctx):
+  await ctx.send("im gonna cuhm on u fr")
+@bot.event
 ## Calls when login is sucessful, prints user info 
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+  print('Logged in as {0.user}'.format(bot))
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
-  
-  if message.content.startswith('!r'):
-    await message.channel.send('Test')
-
-  client.run
+bot.run(os.getenv('TOKEN'))
